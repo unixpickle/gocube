60a5e2085f85142251cc3652c2cb04a70eb0ef8b
Alex Nichol <unixpickle@gmail.com>
2015-01-22 14:14:10 -0500

    eo solver uses new args

--- eo-solve/main.go
+++ eo-solve/main.go
@@ -12,28 +12,32 @@ func main() {
 	// Input the cube.
 	a := args.NewArgs(flag.CommandLine)
 	flag.Parse()
-	scramble, err := a.Scramble()
-	if err != nil {
+	if err := a.Parse(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	scramble := a.Scramble()
 
 	// Setup search variables.
-	moves, _ := gocube.ParseMoves("R U L D R' U' L' D' R2 U2 L2 D2 F B F' B' " +
-		"F2 B2")
+	moves := a.Moves()
 	goal := gocube.EOLineGoal{}
 	start := scramble.Edges
 	fmt.Println("Generating heuristic...")
 	pruner := gocube.MakeEOPruner(moves)
 	
-	for i := 0; i <= 9; i++ {
+	for i := a.MinDepth(); i <= a.MaxDepth(); i++ {
 		fmt.Println("Exploring depth", i, "...")
-		search := start.Search(goal, pruner, moves, i, 1)
-		solution, ok := <-search.Solutions()
-		if ok {
+		search := start.Search(goal, pruner, moves, i, a.Branch())
+		for {
+			solution, ok := <-search.Solutions()
+			if !ok {
+				break
+			}
 			fmt.Println("Found solution:", solution)
-			search.Cancel()
-			break
+			if !a.Multiple() {
+				search.Cancel()
+				return
+			}
 		}
 	}
 }
