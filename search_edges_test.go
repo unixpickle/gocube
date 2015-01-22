3eba6baa04d68903c9be620c6004632293c859fc
Alex Nichol <unixpickle@gmail.com>
2015-01-22 13:10:47 -0500

    benchmark edge search

--- search_edges_test.go
+++ search_edges_test.go
@@ -10,3 +10,21 @@ func BenchmarkMakeEOPruner(b *testing.B) {
 		MakeEOPruner(moves)
 	}
 }
+
+func BenchmarkEdgeSearch(b *testing.B) {
+	moves, _ := ParseMoves("U D F B R L U2 D2 F2 B2 R2 L2 U' D' F' B' R' L'")
+	scramble, _ := ParseMoves("U D F B R")
+	start := SolvedCubieEdges()
+	for _, move := range scramble {
+		start.Move(move)
+	}
+	for i := 0; i < b.N; i++ {
+		s := start.Search(SolveEdgesGoal{}, nil, moves, 5, 0)
+		for {
+			if _, ok := <-s.Solutions(); !ok {
+				break
+			}
+		}
+	}
+}
+
