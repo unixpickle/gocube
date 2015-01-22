747452b899d1a7e0a45332e098d11ab1f8ea1187
Alex Nichol <unixpickle@gmail.com>
2015-01-22 13:32:50 -0500

    added edge search test

--- search_edges_test.go
+++ search_edges_test.go
@@ -28,3 +28,36 @@ func BenchmarkEdgeSearch(b *testing.B) {
 	}
 }
 
+func TestEdgeSearch(t *testing.T) {
+	moves, _ := ParseMoves("U D F B R L U2 D2 F2 B2 R2 L2 U' D' F' B' R' L'")
+	scramble, _ := ParseMoves("R U2 B D F2")
+	start := SolvedCubieEdges()
+	for _, move := range scramble {
+		start.Move(move)
+	}
+	s := start.Search(SolveEdgesGoal{}, nil, moves, len(scramble), 0)
+	solution, ok := <-s.Solutions()
+	if !ok {
+		t.Fatal("Failed to find a solution.")
+	}
+	if len(solution) != len(scramble) {
+		t.Fatal("Invalid solution found:", solution)
+	}
+	for i, m := range solution {
+		invMe := scramble[len(scramble) - (i+1)]
+		if invMe.Turns != 2 {
+			invMe.Turns *= -1
+		}
+		if m.Turns != invMe.Turns || m.Face != invMe.Face {
+			t.Fatal("Invalid solution found:", solution)
+		}
+	}
+	for {
+		solution, ok := <-s.Solutions()
+		if ok {
+			t.Error("Found extraneous solution:", solution)
+		} else {
+			break
+		}
+	}
+}
