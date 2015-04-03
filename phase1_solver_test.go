c1483312cb1f26dc26ddb710f859b6907b2a14d6
Alex Nichol <unixpickle@gmail.com>
2015-04-02 21:01:46 -0400

    basic test for LowerBound()

--- phase1_solver_test.go
+++ phase1_solver_test.go
@@ -1,6 +1,9 @@
 package gocube
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func BenchmarkNewPhase1HeuristicComplete(b *testing.B) {
 	moves := NewPhase1Moves()
@@ -15,5 +18,28 @@ func BenchmarkNewPhase1HeuristicIncomplete(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		NewPhase1Heuristic(moves, false)
+	}
+}
+
+func TestPhase1Heuristic(t *testing.T) {
+	table := NewPhase1Moves()
+	heuristic := NewPhase1Heuristic(table, false)
+	
+	// Do random move sequences and ensure that the lower bound is never too
+	// high.
+	for length := 1; length < 12; length++ {
+		for i := 0; i < 50; i++ {
+			cube := SolvedPhase1Cube()
+			moves := make([]Move, length)
+			for j := 0; j < length; j++ {
+				move := Move(rand.Intn(18))
+				cube.Move(move, table)
+				moves[j] = move
+			}
+			if heuristic.LowerBound(&cube) > length {
+				t.Error("Invalid lower bound", heuristic.LowerBound(&cube),
+					"for moves", moves)
+			}
+		}
 	}
 }
