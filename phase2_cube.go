047150b4a3ba185c347ae3b3d3e38bc05b401f20
Alex Nichol <unixpickle@gmail.com>
2015-04-03 17:51:35 -0400

    drafted SlicePerm and CornerPerm encoding

--- phase2_cube.go
+++ phase2_cube.go
@@ -26,36 +26,99 @@ type Phase2Cube struct {
 func NewPhase2Cube(c CubieCube, axis int) (Phase2Cube, error) {
 	var res Phase2Cube
 	if axis == 0 {
-		res.CornerPermutation = encodeXCornerPerm(&c.Edges)
+		res.CornerPermutation = encodeXCornerPerm(&c.Corners)
+		res.SlicePermutation = encodeMSlicePerm(&c.Edges)
 	} else if axis == 1 {
-		res.CornerPermutation = encodeYCornerPerm(&c.Edges)
+		res.CornerPermutation = encodeYCornerPerm(&c.Corners)
+		res.SlicePermutation = encodeESlicePerm(&c.Edges)
 	} else {
-		res.CornerPermutation = encodeZCornerPerm(&c.Edges)
+		res.CornerPermutation = encodeZCornerPerm(&c.Corners)
+		res.SlicePermutation = encodeSSlicePerm(&c.Edges)
 	}
-	// TODO: figure out edge permutation and slice permutation.
 	return res, nil
 }
 
-func encodeXCornerPerm(e *CubieEdges) int {
+func encodeESlicePerm(e *CubieEdges) int {
+	// Generate a permutation of {0, 1, 2, 3} that represents the permutation of
+	// the E slice.
+	var perm [4]int
+	for i, slot := range []int{1, 3, 7, 9} {
+		// Get the physical piece at the given slot.
+		piece := (*e)[slot].Piece
+		// Get an index from 0 to 3 for the piece.
+		perm[i] = []int{-1, 0, -1, 1, -1, -1, -1, 2, -1, 3, -1, -1}[piece]
+		if perm[i] < 0 {
+			return -1
+		}
+	}
+	return encodePermutationInPlace(perm[:])
+}
+
+func encodeFBEdges(e *CubieEdges) int {
+	// TODO: implement this.
+	return -1
+}
+
+func encodeMSlicePerm(e *CubieEdges) int {
+	// This is like encodeESlicePerm, but for the M slice.
+	var perm [4]int
+	for i, slot := range []int{0, 2, 6, 8} {
+		// Get the physical piece at the given slot.
+		piece := (*e)[slot].Piece
+		// Get an index from 0 to 3 for the piece.
+		perm[i] = []int{0, -1, 1, -1, -1, -1, 2, -1, 3, -1, -1, -1}[piece]
+		if perm[i] < 0 {
+			return -1
+		}
+	}
+	return encodePermutationInPlace(perm[:])
+}
+
+func encodeRLEdges(e *CubieEdges) int {
+	// TODO: implement this.
+	return -1
+}
+
+func encodeSSlicePerm(e *CubieEdges) int {
+	// This is like encodeESlicePerm, but for the S slice.
+	var perm [4]int
+	for i, slot := range []int{11, 10, 5, 4} {
+		// Get the physical piece at the given slot.
+		piece := (*e)[slot].Piece
+		// Get an index from 0 to 3 for the piece.
+		perm[i] = []int{-1, -1, -1, -1, 3, 2, -1, -1, -1, -1, 1, 0}[piece]
+		if perm[i] < 0 {
+			return -1
+		}
+	}
+	return encodePermutationInPlace(perm[:])
+}
+
+func encodeUDEdges(e *CubieEdges) int {
+	// TODO: implement this.
+	return -1
+}
+
+func encodeXCornerPerm(c *CubieCorners) int {
 	var perm [8]int
 	for i, idx := range xCornerIndices {
-		perm[i] = inverseXCornerIndices[(*e)[idx].Piece]
+		perm[i] = inverseXCornerIndices[(*c)[idx].Piece]
 	}
 	return encodePermutationInPlace(perm[:])
 }
 
-func encodeYCornerPerm(e *CubieEdges) int {
+func encodeYCornerPerm(c *CubieCorners) int {
 	var perm [8]int
 	for i := 0; i < 8; i++ {
-		perm[i] = (*e)[i].Piece
+		perm[i] = (*c)[i].Piece
 	}
 	return encodePermutationInPlace(perm[:])
 }
 
-func encodeZCornerPerm(e *CubieEdges) int {
+func encodeZCornerPerm(c *CubieCorners) int {
 	var perm [8]int
 	for i, idx := range zCornerIndices {
-		perm[i] = inverseZCornerIndices[(*e)[idx].Piece]
+		perm[i] = inverseZCornerIndices[(*c)[idx].Piece]
 	}
 	return encodePermutationInPlace(perm[:])
 }
